dc3318a99a367314ae91c50a482d7f85eb73f07b
Aleksander Hurtley <aleksander.hurtley@schibsted.com>
2020-07-03 11:10:26 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
         <Layout>
           <Router>
             <Switch>
-              <Route exact path="aleksanderhurtley.github.io/smbArkaden/" component={Home} />
-              <Route path="aleksanderhurtley.github.io/smbArkaden/games" component={Games} />
-              <Route path="aleksanderhurtley.github.io/smbArkaden/about" component={About} />
+              <Route path="/" component={Home} />
+              <Route path="/games" component={Games} />
+              <Route path="/about" component={About} />
               <Route component={NoMatch} />
             </Switch>
           </Router>
