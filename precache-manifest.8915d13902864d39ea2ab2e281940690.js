47bae8dfa70d7612a1bde7d55f91694336943ad9
Aleksander Hurtley <aleksander.hurtley@schibsted.com>
2020-07-03 10:46:26 +0200

    Updates

---
+++ precache-manifest.8915d13902864d39ea2ab2e281940690.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4cf82d4b6c2a606c6b7e12b4709a05",
+    "url": "/smbArkaden/index.html"
+  },
+  {
+    "revision": "58d18ea4ddf8b600c1b7",
+    "url": "/smbArkaden/static/css/main.73e78f0f.chunk.css"
+  },
+  {
+    "revision": "35eed4f47e2773b58a21",
+    "url": "/smbArkaden/static/js/2.a73f7bfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/smbArkaden/static/js/2.a73f7bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d18ea4ddf8b600c1b7",
+    "url": "/smbArkaden/static/js/main.041be759.chunk.js"
+  },
+  {
+    "revision": "aac097858ba62871b933",
+    "url": "/smbArkaden/static/js/runtime-main.58f3c1d9.js"
+  },
+  {
+    "revision": "9a814d58e398b3c2cd7c1a5d2509b661",
+    "url": "/smbArkaden/static/media/arcade2.9a814d58.jpg"
+  },
+  {
+    "revision": "5fcd6c657ffa8a6791632c705f0944ea",
+    "url": "/smbArkaden/static/media/arcadeFont.5fcd6c65.ttf"
+  },
+  {
+    "revision": "f0edd1e37a6bbdd7792810bf491cd905",
+    "url": "/smbArkaden/static/media/fasade.f0edd1e3.jpg"
+  },
+  {
+    "revision": "9e29f353c1c110a542c257896a3f0a96",
+    "url": "/smbArkaden/static/media/scavengerbanner.9e29f353.jpg"
+  },
+  {
+    "revision": "b37ee9449b4d38dddd2140bd69a66e49",
+    "url": "/smbArkaden/static/media/sinusbanner.b37ee944.jpg"
+  },
+  {
+    "revision": "d6d3b281ac8c730beeac2931f8e14550",
+    "url": "/smbArkaden/static/media/tetrisbanner.d6d3b281.jpg"
+  }
+]);
