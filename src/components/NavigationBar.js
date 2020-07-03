e3104ee650371d508fe3413b4c5b354fd4c15d0e
Aleksander Hurtley <aleksander.hurtley@schibsted.com>
2020-07-03 13:13:21 +0200

    Routing

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -23,12 +23,12 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/">SMB Arcade</Navbar.Brand>
+      <Navbar.Brand href="/#/">SMB Arcade</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-Navbar.nav"/>
       <Navbar.Collapse id="basic-Navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link href="/games">Arcade Hall</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/#/">Home</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/#/games">Arcade Hall</Nav.Link></Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
