571dc05186725b09178eb30f1b3bebed8c067037
Aleksander Hurtley <aleksander.hurtley@schibsted.com>
2020-08-28 10:46:12 +0200

    reset react app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home } from './Home'
-import { About } from './About'
-import { Games } from './Games'
-import { NoMatch } from './NoMatch'
-import { Layout } from './components/Layout'
-import { NavigationBar } from './components/NavigationBar';
-import { Jumbotron } from './components/Jumbotron';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavigationBar/>
-        <Jumbotron />
-        <Layout>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/games" component={Games} />
-              <Route path="/about" component={About} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
-        </Layout>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
