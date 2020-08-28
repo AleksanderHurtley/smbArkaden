cd80ed8de4c6f4b5e432cd084c03c5bc7c276b5d
Aleksander Hurtley <aleksander.hurtley@schibsted.com>
2020-08-28 13:28:31 +0200

    Bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -3,29 +3,34 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import {Home as Home} from './Home';
 import {About as About}from './About';
 import { Games as Games } from './Games';
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button  } from 'react-bootstrap';
  
 class App extends Component {
   render() {
     return (
-    <Router>
-        <div>
-          <h2>SMB Arkaden</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/games'} className="nav-link">Games</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-          </nav>
-          <hr />
-          <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route path='/games' component={Games} />
-          </Switch>
-        </div>
-      </Router>
-    );
+      <div className="App">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">SMB Arkaden</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/games">Games</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Router>
+          <div>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/games' component={Games} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
   }
 }
 
